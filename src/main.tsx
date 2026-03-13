bf86dabfc4bccbd4ad0f41fc73081d3629e8f7f8
RuthwikKakumani <ruthwikkakumani08@gmail.com>
2026-03-13 17:03:20 +0530

    Integrated Vercel Speed Insights

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import ReactDOM from "react-dom/client";
+import { injectSpeedInsights } from '@vercel/speed-insights';
 import App from "./App";
 import "../index.css";
+
+injectSpeedInsights();
 
 const queryClient = new QueryClient();
 
